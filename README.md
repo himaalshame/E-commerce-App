c9096b04c45654badf5d729d0166be7d5df8cf10
himaa <himaalshame53@gmail.com>
2024-03-10 11:18:00 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Getting Started with Basic E-Commerce-App
+
+first Clone / Download The Project
+cd into the project directory then you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Navbar from "./compoenents/Navbar";
+import ProductsList from "./compoenents/ProductsList";
+import Slider from "./compoenents/Slider";
+import { Routes, Route } from "react-router-dom";
+import About from "./compoenents/About";
+import ProductDetails from "./compoenents/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Slider />
+              <ProductsList />
+            </>
+          }
+        />
+        <Route path="about" element={<About />} />
+        <Route path="product/:productId" element={<ProductDetails />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
